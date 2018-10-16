283925f0df567e7e25989e4c4045895e95ca980f
Victoria L Sumner <vicsumner@gmail.com>
2018-10-16 22:33:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-return Object.assign({object}, { key: 'value' })
+return Object.assign({}, object, { [key]: value })
 }
 
-// const object = { key: value }
- 
-// updateObjectWithKeyAndValue(oject, 'key', 'value')

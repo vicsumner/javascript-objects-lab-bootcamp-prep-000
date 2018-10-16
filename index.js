15d45e94eed8bb00d44f3c3850e09ade0ce4d1a0
Victoria L Sumner <vicsumner@gmail.com>
2018-10-16 22:29:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  // object[key] = value
- Object.assign({object}, { key: 'value' })
-  return object
+  object[key] = value
+  // return object
 }
 
-
+Object.assign({object}, { key: 'value' })
 
 // const object = { key: value }
  

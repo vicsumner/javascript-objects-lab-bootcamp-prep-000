4bd2ae4c4dbcbf63885794a24edbb8c427e8896a
Victoria L Sumner <vicsumner@gmail.com>
2018-10-17 00:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
    var newObject = Object.assign({}, object)
-   delete newObject[key]
+   delete newObject.key
    return newObject
 }
 
